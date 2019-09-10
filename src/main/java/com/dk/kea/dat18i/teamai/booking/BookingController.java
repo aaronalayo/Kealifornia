5e569c2a947321e04ee909b21b0ae984af91c0f1
aAron <aaron@stud.kea.dk>
2019-09-10 14:04:17 +0200

    booking and gitignore

--- src/main/java/com/dk/kea/dat18i/teamai/booking/BookingController.java
+++ src/main/java/com/dk/kea/dat18i/teamai/booking/BookingController.java
@@ -1,5 +1,6 @@
 package com.dk.kea.dat18i.teamai.booking;
 
+import com.dk.kea.dat18i.teamai.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,17 +14,50 @@ public class BookingController {
     BookingRepo bookingRepo;
 
     @GetMapping("/bookings")
-        public String findAll(Model model){
-            List<Booking> bookingList = bookingRepo.findAll();
-            model.addAttribute("Bookings", bookingList);
-            return "show-bookings";
+
+    public String booking(Model model) {
+        List<Booking> bookingList = bookingRepo.findAll();
+        model.addAttribute("bookings", bookingList);
+
+        return "show-bookings";
+
+
     }
 
-    @PostMapping("/bookings/{id}")
-    public int saveOne(@RequestBody Booking newBooking){
-    int bookingId=bookingRepo.addBooking(newBooking);
-    return bookingId;
+    @GetMapping("/bookroom")
+    public String saveOne(Model model){
+
+        Booking newBooking = new Booking();
+        model.addAttribute(newBooking);
+
+    return "book-room";
     }
+
+    @PostMapping("/savebooking")
+    public String saveBooking(@ModelAttribute Booking booking){
+
+        Booking newBooking = new Booking();
+
+        newBooking.setCheck_in(booking.getCheck_in());
+        newBooking.setCheck_out(booking.getCheck_out());
+        newBooking.setPersons(booking.getPersons());
+        newBooking.setNumber_of_rooms(booking.getNumber_of_rooms());
+        newBooking.setCustomer(booking.getCustomer());
+        newBooking.setRoom(booking.getRoom());
+
+
+        bookingRepo.addBooking(newBooking);
+
+        return "redirect:/customerinfo";
+    }
+   @GetMapping("/customerinfo")
+   public String customerInfo(Model model){
+
+        Customer newCustomer= new Customer();
+        model.addAttribute(newCustomer);
+
+        return "customer-info";
+   }
 
     @PutMapping("/bookings/{id}")
     public void updateOne(@PathVariable int id, @RequestBody Booking booking){
@@ -35,6 +69,8 @@ public class BookingController {
         bookingRepo.deleteOne(id);
 
     }
+
+
 
 
 }
--- src/main/java/com/dk/kea/dat18i/teamai/rooms/RoomsController.java
+++ src/main/java/com/dk/kea/dat18i/teamai/rooms/RoomsController.java
@@ -1,5 +1,6 @@
 package com.dk.kea.dat18i.teamai.rooms;
 
+import com.dk.kea.dat18i.teamai.booking.BookingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,8 @@ public class RoomsController {
 
     @Autowired
     private RoomsRepository roomsRepo;
+
+    @Autowired
 
     @GetMapping("/roomview")
     public Rooms showRoom(){
